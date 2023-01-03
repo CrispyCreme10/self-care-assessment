b46a2dd79f57a19e5b6d8b36a1bd16c87fe5de2e
CrispyCreme10 <jordanrenner69@gmail.com>
2023-01-02 21:20:27 -0600

    switch up style

--- src/App.js
+++ src/App.js
@@ -173,46 +173,45 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Self-Care Assessment</h1>
+      <div className="panel details-panel">
+        <h1>Self-Care Assessment</h1>
 
-      <div id="text">
-        <p><span>Self-care</span> activities are the things you do to maintain good health and improve well-being. You'll  find that many of these activities are things you already do as part of your normal routine.</p>
-        <p>In this assessment you will think about how frequently, or how well, you are performing different  self-care activities. The goal of this assessment is to help you learn about your self-care needs  by spotting patterns and recognizing areas of your life that need more attention.</p>
-        <p>There are no right or wrong answers on this assessment. There may be activities that you have  no interest in, and other activities may not be included. This list is not comprehensive, but serves  as a starting point for thinking about your self-care needs.</p>
+        <div id="text">
+          <p><span>Self-care</span> activities are the things you do to maintain good health and improve well-being. You'll  find that many of these activities are things you already do as part of your normal routine.</p>
+          <p>In this assessment you will think about how frequently, or how well, you are performing different  self-care activities. The goal of this assessment is to help you learn about your self-care needs  by spotting patterns and recognizing areas of your life that need more attention.</p>
+          <p>There are no right or wrong answers on this assessment. There may be activities that you have  no interest in, and other activities may not be included. This list is not comprehensive, but serves  as a starting point for thinking about your self-care needs.</p>
+        </div>
+
+        <div className="legend">
+          <div className="legend-row">
+            <span className="key">1</span>
+            <span className="desc-1">I do this poorly</span>
+            <span className="desc-2">I do this rarely or not at all</span>
+          </div>
+          <div className="legend-row">
+            <span className="key">2</span>
+            <span className="desc-1">I do this OK</span>
+            <span className="desc-2">I do this sometimes</span>
+          </div>
+          <div className="legend-row">
+            <span className="key">3</span>
+            <span className="desc-1">I do this well</span>
+            <span className="desc-2">I do this often</span>
+          </div>
+          <div className="legend-row">
+            <span className="key">★</span>
+            <span className="desc-1">I would like to improve at this</span>
+            <span className="desc-2">I would like to do this more frequently</span>
+          </div>
+        </div>
       </div>
-      
-      <div className="legend">
-        <div className="legend-row">
-          <span className="key">1</span>
-          <span className="desc-1">I do this poorly</span>
-          <span className="desc-2">I do this rarely or not at all</span>
-        </div>
-        <div className="legend-row">
-          <span className="key">2</span>
-          <span className="desc-1">I do this OK</span>
-          <span className="desc-2">I do this sometimes</span>
-        </div>
-        <div className="legend-row">
-          <span className="key">3</span>
-          <span className="desc-1">I do this well</span>
-          <span className="desc-2">I do this often</span>
-        </div>
-        <div className="legend-row">
-          <span className="key">★</span>
-          <span className="desc-1">I would like to improve at this</span>
-          <span className="desc-2">I would like to do this more frequently</span>
-        </div>
-      </div>
 
-      <InfoTable categoryName={"Physical Self-Care"} lineItems={lineItems1} />
-      <InfoTable categoryName={"Psychological / Emotional Self-Care"} lineItems={lineItems2} />
-      <InfoTable categoryName={"Social Self-Care"} lineItems={lineItems3} />
-      <InfoTable categoryName={"Spiritual Self-Care"} lineItems={lineItems4} />
-      <InfoTable categoryName={"Professional Self-Care"} lineItems={lineItems5} />
-
-      <div>
-        <button>Save</button>
-        <button>Clear All</button>
+      <div className="panel data-panel">
+        <InfoTable categoryName={"Physical Self-Care"} lineItems={lineItems1} />
+        <InfoTable categoryName={"Psychological / Emotional Self-Care"} lineItems={lineItems2} />
+        <InfoTable categoryName={"Social Self-Care"} lineItems={lineItems3} />
+        <InfoTable categoryName={"Spiritual Self-Care"} lineItems={lineItems4} />
+        <InfoTable categoryName={"Professional Self-Care"} lineItems={lineItems5} />
       </div>
 
     </div>
